2e568d0f47dd7e3da0fa6f9bf5c4c43ff5c8fd1e
Fabio Moggi <fabio.moggi@gmail.com>
2026-06-25 12:52:37 -0300

    chore: fix sonarqube reliability and maintainability issues

--- src/user/application/ports/auth-provider.port.ts
+++ src/user/application/ports/auth-provider.port.ts
@@ -1,6 +1,4 @@
-import type { UserId } from "../../domain/value-objects/index.js";
-import type { Email } from "../../domain/value-objects/index.js";
-import type { Password } from "../../domain/value-objects/index.js";
+import type { UserId, Email, Password } from "../../domain/value-objects/index.js";
 
 export interface AuthProviderPort {
   accountExistsById(id: UserId): Promise<boolean>;
--- src/user/application/ports/user-id-deriver.port.ts
+++ src/user/application/ports/user-id-deriver.port.ts
@@ -1,5 +1,4 @@
-import type { Email } from "../../domain/value-objects/index.js";
-import type { UserId } from "../../domain/value-objects/index.js";
+import type { Email, UserId } from "../../domain/value-objects/index.js";
 
 export interface UserIdDeriverPort {
   derive(email: Email): Promise<UserId>;
